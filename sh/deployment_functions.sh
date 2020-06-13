9f5f1323d7110d77f0589eb800867c0cad032cb4
Jon Jagger <jon@jaggersoft.com>
2020-06-13 10:47:52 +0100

    Put all deployment functions into one curlable file

---
+++ sh/deployment_functions.sh
@@ -0,0 +1,114 @@
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+gcloud_init()
+{
+  # vars are in ci context
+
+  echo ${GCP_K8S_CREDENTIALS} > /gcp/gcp-credentials.json
+
+  gcloud auth activate-service-account \
+    "${SERVICE_ACCOUNT}" \
+    --key-file=/gcp/gcp-credentials.json
+
+  gcloud container clusters get-credentials \
+    "${CLUSTER}" \
+    --zone "${ZONE}" \
+    --project "${PROJECT}"
+}
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+helm_init()
+{
+  helm init --client-only
+  helm repo add praqma https://praqma-helm-repo.s3.amazonaws.com/
+}
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+helm_chart_version()
+{
+  echo 0.2.5
+}
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+helm_upgrade_probe_no_prometheus_no()
+{
+  local -r namespace="${1}"
+  local -r repo="${2}"
+  local -r image="${3}"
+  local -r tag="${4}"
+  local -r port="${5}"
+  local -r general_values="${6}"
+  if [ -z "${7:-}" ]; then
+    local -r specific_values=""
+  else
+    local -r specific_values="--values ${7}"
+  fi
+
+  helm upgrade \
+    --install \
+    --namespace=${namespace} \
+    --set-string containers[0].image=${image} \
+    --set-string containers[0].tag=${tag} \
+    --set service.port=${port} \
+    --values ${general_values} \
+    ${specific_values} \
+    ${namespace}-${repo} \
+    praqma/cyber-dojo-service \
+    --version "$(helm_chart_version)"
+}
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+helm_upgrade_probe_yes_prometheus_no()
+{
+  local -r namespace="${1}"
+  local -r repo="${2}"
+  local -r image="${3}"
+  local -r tag="${4}"
+  local -r port="${5}"
+  local -r general_values="${6}"
+  local -r specific_values="${7}"
+
+  helm upgrade \
+    --install \
+    --namespace=${namespace} \
+    --set-string containers[0].image=${image} \
+    --set-string containers[0].tag=${tag} \
+    --set service.port=${port} \
+    --set-string service.annotations."prometheus\.io/port"=${port} \
+    --values ${general_values} \
+    --values ${specific_values} \
+    ${namespace}-${repo} \
+    praqma/cyber-dojo-service \
+    --version "$(helm_chart_version)"
+}
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+helm_upgrade_probe_yes_prometheus_yes()
+{
+  local -r namespace="${1}"
+  local -r repo="${2}"
+  local -r image="${3}"
+  local -r tag="${4}"
+  local -r port="${5}"
+  local -r general_values="${6}"
+  if [ -z "${7:-}" ]; then
+    local -r specific_values=""
+  else
+    local -r specific_values="--values ${7}"
+  fi
+
+  helm upgrade \
+    --install \
+    --namespace=${namespace} \
+    --set-string containers[0].image=${image} \
+    --set-string containers[0].tag=${tag} \
+    --set service.port=${port} \
+    --set containers[0].livenessProbe.port=${port} \
+    --set containers[0].readinessProbe.port=${port} \
+    --set-string service.annotations."prometheus\.io/port"=${port} \
+    --values ${general_values} \
+    ${specific_values} \
+    ${namespace}-${repo} \
+    praqma/cyber-dojo-service \
+    --version "$(helm_chart_version)"
+}
