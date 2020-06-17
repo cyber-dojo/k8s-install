b8a8115100cc8bb107883a45d96f38fa27427417
Jon Jagger <jon@jaggersoft.com>
2020-06-17 20:01:26 +0100

    Abandon attempt to fight bash apparantly infinite awkwardness

--- sh/deployment_functions.sh
+++ sh/deployment_functions.sh
@@ -27,54 +27,6 @@ helm_init()
 }
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-helm_upgrade()
-{
-  local -r namespace="${1}"
-  local -r repo="${2}"
-  local -r image="${3}"
-  local -r tag="${4}"
-  local -r port="${5}"
-  local -r general_values="--values ${6}"
-
-  set -x
-  if [ -z "${7:-}" ]; then
-    local -r specific_values=""
-  else
-    local -r specific_values="--values ${7}"
-  fi
-
-  if [ "${K8S_SET_PROMETHEUS:-x}" == "false" ]; then
-    local -r k8s_prometheus=""
-  else
-    local dqt='"'
-    local -r k8s_prometheus="--set-string service.annotations.${dqt}prometheus\.io/port${dqt}=${port}"
-  fi
-
-  if [ "${K8S_SET_PROBES:-x}" == "false" ]; then
-    local -r k8s_liveness_probe=""
-    local -r k8s_readiness_probe=""
-  else
-    local -r k8s_liveness_probe="--set containers[0].livenessProbe.port=${port}"
-    local -r k8s_readiness_probe="--set containers[0].readinessProbe.port=${port}"
-  fi
-
-  helm upgrade \
-    --install \
-    --namespace=${namespace} \
-    --set-string containers[0].image=${image} \
-    --set-string containers[0].tag=${tag} \
-    --set service.port=${port} \
-    ${k8s_liveness_probe} \
-    ${k8s_readiness_probe} \
-    ${k8s_prometheus} \
-    ${general_values} \
-    ${specific_values} \
-    ${namespace}-${repo} \
-    ${HELM_CHART_REPO} \
-    --version ${HELM_CHART_VERSION}
-}
-
-#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 helm_upgrade_probe_no_prometheus_no()
 {
   local -r namespace="${1}"
@@ -103,6 +55,11 @@ helm_upgrade_probe_no_prometheus_no()
 }
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+helm_upgrade_probe_no_prometheus_yes()
+{
+  helm_upgrade_probe_yes_prometheus_no
+}
+
 helm_upgrade_probe_yes_prometheus_no()
 {
   local -r namespace="${1}"
