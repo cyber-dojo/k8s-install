9cf265d0f6c7b533d9e03a433c1c3a9c51acccbf
Neethu Philip <neethuphilip@Neethus-MacBook-Pro.local>
2020-08-05 14:24:26 +0200

    WIP

--- .circleci/utils.sh
+++ .circleci/utils.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Script to install k3s in machine executor
+HELM_VERSION = $1
 
 function install_k3s(){
-
+    echo $PATH
     echo "download and install k3s"
     curl -sfL https://get.k3s.io | sh -
     k3s --help
@@ -12,5 +13,16 @@ function install_k3s(){
 
 }
 
+function install_helm(){
+    echo "download and install helm"
+    curl -Lo helm-${HELM_VERSION}-linux-amd64.tar.gz https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz && \
+    tar -zxvf helm-${HELM_VERSION}-linux-amd64.tar.gz && \
+    mv linux-amd64/helm /usr/local/bin/helm && \
+    rm helm-${HELM_VERSION}-linux-amd64.tar.gz && \
+    rm -rf linux-amd64
+}
+
 echo "install k3s"
 install_k3s
+echo "install helm"
+install_helm
