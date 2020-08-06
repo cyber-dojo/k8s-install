45c9b935e6f19232c753f3d1fa751ad540b2f4a8
Neethu Philip <neethuphilip@Neethus-MacBook-Pro.local>
2020-08-06 12:09:20 +0200

    WIP

--- build_scripts/utils.sh
+++ build_scripts/utils.sh
@@ -6,13 +6,8 @@ function install_k3s(){
     echo $PATH
     echo "download and install k3s"
     sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode 664" sh -
-    sleep 10
-    sudo k3s kubectl get nodes --all-namespaces
+    sleep 25
     kubectl get nodes -A
-    kubectl get pods -A
-    sleep 10
-    kubectl get nodes -A
-
 }
 
 function install_helm(){
