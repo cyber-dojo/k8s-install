749c3d704762116d7dd2b8165d5bff4d2c795b20
Neethu Philip <neethuphilip@Neethus-MacBook-Pro.local>
2020-08-06 15:55:55 +0200

    WIP

--- build_scripts/utils.sh
+++ build_scripts/utils.sh
@@ -7,6 +7,8 @@ function install_k3s(){
     echo "download and install k3s"
     sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode 664" sh -
     export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+    echo "KUBECONFIG = $KUBECONFIG"
+    cat /etc/rancher/k3s/k3s.yaml
     sleep 25
     kubectl get nodes -A
 
