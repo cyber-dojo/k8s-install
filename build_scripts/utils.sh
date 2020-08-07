65e60f8daab50f7572731252c43802e10dd423d4
Neethu Philip <neethuphilip@Neethus-MacBook-Pro.local>
2020-08-07 10:19:53 +0200

    WIP

--- build_scripts/utils.sh
+++ build_scripts/utils.sh
@@ -8,13 +8,12 @@ function install_k3s(){
     sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode 664" sh -
     export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
     echo "KUBECONFIG = $KUBECONFIG"
-    echo "Home is $HOME"
-    cd
-    cd .kube/
+    ls -al
+
+    cd ~/.kube/
     ls -al
     sleep 25
     kubectl get nodes -A
-    kubectl cluster-info
 
 }
 
