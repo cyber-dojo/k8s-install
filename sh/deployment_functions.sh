72158eea58625d511a94586cdbfeb593ab52dcf1
Jon Jagger <jon@jaggersoft.com>
2021-05-15 10:55:28 +0100

    [ci skip] Delete dead code

--- sh/deployment_functions.sh
+++ sh/deployment_functions.sh
@@ -57,31 +57,6 @@ helm_upgrade_probe_no_prometheus_no()
 }
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-helm_upgrade_probe_no_prometheus_yes()
-{
-  local -r namespace="${1}"
-  local -r repo="${2}"
-  local -r image="${3}"
-  local -r tag="${4}"
-  local -r port="${5}"
-  local -r general_values="${6}"
-  local -r specific_values="${7}"
-
-  helm upgrade \
-    --install \
-    --namespace=${namespace} \
-    --set-string containers[0].image=${image} \
-    --set-string containers[0].tag=${tag} \
-    --set service.port=${port} \
-    --set-string service.annotations."prometheus\.io/port"=${port} \
-    --values ${general_values} \
-    --values ${specific_values} \
-    ${namespace}-${repo} \
-    ${HELM_CHART_REPO} \
-    --version ${HELM_CHART_VERSION}
-}
-
-#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 helm_upgrade_probe_yes_prometheus_yes()
 {
   local -r namespace="${1}"
